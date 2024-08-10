73cfd0eeb6052caa8792e182c1736e06d39131f7
MeetB7 <meetbrijwani@gmail.com>
2024-08-10 23:11:14 +0530

    fe

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -1,11 +1,9 @@
 import Link from "next/link";
 import GradualSpacing from "./magicui/gradual-spacing";
-import { auth } from "@/app/api/auth/[...nextauth]/auth";
 import Image from "next/image";
 
 
 export async function Landing() {
-  const session = await auth()
   return (
     <section id="landing" className="w-full py-12 md:py-24 lg:py-32 ">
       <div className="container px-4 md:px-6 max-sm:mt-10">
@@ -28,7 +26,7 @@ export async function Landing() {
             </div>
             <div className="flex flex-col gap-2 min-[400px]:flex-row">
               <Link
-                href="/api/auth/signin"
+                href=""
                 className="inline-flex h-10 items-center justify-center rounded-md bg-primary px-8 text-sm font-medium 
                 text-primary-foreground shadow transition-colors hover:bg-primary/90 focus-visible:outline-none 
                 focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
@@ -37,7 +35,7 @@ export async function Landing() {
                 Sign In
               </Link>
               <Link
-                href="/api/auth/signout"
+                href=""
                 className="inline-flex h-10 items-center justify-center rounded-md border border-input bg-background px-8 text-sm font-medium shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50"
                 prefetch={false}
               >
@@ -52,11 +50,6 @@ export async function Landing() {
             alt="Hero"
             className="mx-auto aspect-video overflow-hidden rounded-xl object-fit sm:w-full lg:order-last lg:aspect-square"
           ></Image>
-          {session? (
-            <div className="fixed bg-muted bottom-1 right-1 rounded-xl px-3">Logged In</div>
-          ):(
-            <></>
-          )}
         </div>
       </div>
     </section>
